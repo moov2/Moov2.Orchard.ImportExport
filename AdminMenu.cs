0675ae95d5e5b141138caccb1e894fa3968b677f
Lawrence Dine <lawrence@moov2.com>
2017-12-13 13:19:26 +0000

    Add new tab to ImportExport that links to new blank route in new AdminController

---
+++ AdminMenu.cs
@@ -0,0 +1,24 @@
+﻿using Orchard.ImportExport;
+using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace Moov2.Orchard.ImportExport
+{
+    public class AdminMenu : INavigationProvider
+    {
+        public Localizer T { get; set; }
+
+        public string MenuName => "admin";
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.AddImageSet("importexport")
+                .Add(T("Import/Export"), "42", BuildMenu);
+        }
+
+        private void BuildMenu(NavigationItemBuilder menu)
+        {
+            menu.Add(T("Export Content"), "0", item => item.Action("ExportContent", "Admin", new { area = "Moov2.Orchard.ImportExport" }).Permission(Permissions.Export).LocalNav());
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Moov2.Orchard.ImportExport.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult ExportContent()
+        {
+            return new HttpNotFoundResult();
+        }
+    }
+}
