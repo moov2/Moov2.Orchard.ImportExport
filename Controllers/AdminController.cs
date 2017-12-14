40834fc36a68685218c6bb152bee807755939679
Joe B <Joe@Moov2.com>
2017-12-14 09:17:17 +0000

    Add basic content grid to custom export content tab

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,159 @@
-﻿using System.Web.Mvc;
+﻿using Orchard;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Models;
+using Orchard.Core.Common.Models;
+using Orchard.Core.Contents;
+using Orchard.Core.Contents.Settings;
+using Orchard.Core.Contents.ViewModels;
+using Orchard.DisplayManagement;
+using Orchard.ImportExport.Services;
+using Orchard.Localization.Services;
+using Orchard.Mvc;
+using Orchard.Settings;
+using Orchard.UI.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Moov2.Orchard.ImportExport.Controllers
 {
     public class AdminController : Controller
     {
-        public ActionResult ExportContent()
+
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly IContentManager _contentManager;
+        private readonly ICultureFilter _cultureFilter;
+        private readonly ICultureManager _cultureManager;
+        private readonly ICustomExportStep _customExportStep;
+        private readonly ISiteService _siteService;
+
+        public AdminController(ICustomExportStep customExportStep, IContentDefinitionManager contentDefinitionManager, IContentManager contentManager, ISiteService siteService, ICultureFilter cultureFilter, ICultureManager cultureManager, IShapeFactory shapeFactory, IOrchardServices orchardServices)
         {
-            return new HttpNotFoundResult();
+            _contentDefinitionManager = contentDefinitionManager;
+            _contentManager = contentManager;
+            _cultureFilter = cultureFilter;
+            _cultureManager = cultureManager;
+            _customExportStep = customExportStep;
+            _siteService = siteService;
+
+            Shape = shapeFactory;
+            Services = orchardServices;
+        }
+
+        dynamic Shape { get; set; }
+        public IOrchardServices Services { get; private set; }
+
+        public ActionResult ExportContent(ListContentsViewModel model, PagerParameters pagerParameters)
+        {
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+
+            var versionOptions = VersionOptions.Latest;
+            switch (model.Options.ContentsStatus)
+            {
+                case ContentsStatus.Published:
+                    versionOptions = VersionOptions.Published;
+                    break;
+                case ContentsStatus.Draft:
+                    versionOptions = VersionOptions.Draft;
+                    break;
+                case ContentsStatus.AllVersions:
+                    versionOptions = VersionOptions.AllVersions;
+                    break;
+                default:
+                    versionOptions = VersionOptions.Latest;
+                    break;
+            }
+
+            var query = _contentManager.Query(versionOptions, GetListableTypes(false).Select(ctd => ctd.Name).ToArray());
+
+            if (!string.IsNullOrEmpty(model.TypeName))
+            {
+                var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(model.TypeName);
+                if (contentTypeDefinition == null)
+                    return HttpNotFound();
+
+                model.TypeDisplayName = !string.IsNullOrWhiteSpace(contentTypeDefinition.DisplayName)
+                                            ? contentTypeDefinition.DisplayName
+                                            : contentTypeDefinition.Name;
+                query = query.ForType(model.TypeName);
+            }
+
+            switch (model.Options.OrderBy)
+            {
+                case ContentsOrder.Modified:
+                    //query = query.OrderByDescending<ContentPartRecord, int>(ci => ci.ContentItemRecord.Versions.Single(civr => civr.Latest).Id);
+                    query = query.OrderByDescending<CommonPartRecord>(cr => cr.ModifiedUtc);
+                    break;
+                case ContentsOrder.Published:
+                    query = query.OrderByDescending<CommonPartRecord>(cr => cr.PublishedUtc);
+                    break;
+                case ContentsOrder.Created:
+                    //query = query.OrderByDescending<ContentPartRecord, int>(ci => ci.Id);
+                    query = query.OrderByDescending<CommonPartRecord>(cr => cr.CreatedUtc);
+                    break;
+            }
+
+            if (!String.IsNullOrWhiteSpace(model.Options.SelectedCulture))
+            {
+                query = _cultureFilter.FilterCulture(query, model.Options.SelectedCulture);
+            }
+
+            model.Options.SelectedFilter = model.TypeName;
+            model.Options.FilterOptions = GetListableTypes(false)
+                .Select(ctd => new KeyValuePair<string, string>(ctd.Name, ctd.DisplayName))
+                .ToList().OrderBy(kvp => kvp.Value);
+
+            model.Options.Cultures = _cultureManager.ListCultures();
+
+            var maxPagedCount = _siteService.GetSiteSettings().MaxPagedCount;
+            if (maxPagedCount > 0 && pager.PageSize > maxPagedCount)
+                pager.PageSize = maxPagedCount;
+            var pagerShape = Shape.Pager(pager).TotalItemCount(maxPagedCount > 0 ? maxPagedCount : query.Count());
+            var pageOfContentItems = query.Slice(pager.GetStartIndex(), pager.PageSize).ToList();
+
+            var list = Shape.List();
+            list.AddRange(pageOfContentItems.Select(ci => _contentManager.BuildDisplay(ci, "SummaryAdmin")));
+
+            var viewModel = Shape.ViewModel()
+                .ContentItems(list)
+                .Pager(pagerShape)
+                .Options(model.Options)
+                .TypeDisplayName(model.TypeDisplayName ?? "");
+
+            return View(viewModel);
+        }
+
+        private IEnumerable<ContentTypeDefinition> GetListableTypes(bool andContainable)
+        {
+            return _contentDefinitionManager.ListTypeDefinitions().Where(ctd =>
+                Services.Authorizer.Authorize(Permissions.EditContent, _contentManager.New(ctd.Name)) &&
+                ctd.Settings.GetModel<ContentTypeSettings>().Listable &&
+                (!andContainable || ctd.Parts.Any(p => p.PartDefinition.Name == "ContainablePart")));
+        }
+
+        [HttpPost, ActionName("List")]
+        [FormValueRequired("submit.Filter")]
+        public ActionResult ListFilterPOST(ContentOptions options)
+        {
+            var routeValues = ControllerContext.RouteData.Values;
+            if (options != null)
+            {
+                routeValues["Options.SelectedCulture"] = options.SelectedCulture; //todo: don't hard-code the key
+                routeValues["Options.OrderBy"] = options.OrderBy; //todo: don't hard-code the key
+                routeValues["Options.ContentsStatus"] = options.ContentsStatus; //todo: don't hard-code the key
+                if (GetListableTypes(false).Any(ctd => string.Equals(ctd.Name, options.SelectedFilter, StringComparison.OrdinalIgnoreCase)))
+                {
+                    routeValues["id"] = options.SelectedFilter;
+                }
+                else
+                {
+                    routeValues.Remove("id");
+                }
+            }
+
+            return RedirectToAction("List", routeValues);
         }
     }
 }
---
+++ ViewModels/ExportContentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Orchard.ContentManagement;
+using System.Collections.Generic;
+
+namespace Orchard.ImportExport.ViewModels
+{
+    public class ExportContentViewModel {
+        public IList<ContentItem> ContentItems { get; set; }
+        public IList<CustomStepEntry> CustomSteps { get; set; }
+        public virtual bool Metadata { get; set; }
+        public virtual bool Data { get; set; }
+        public virtual string DataImportChoice { get; set; }
+        public virtual bool SiteSettings { get; set; }
+    }
+
+    public class ContentTypeEntry {
+        public string ContentTypeName { get; set; }
+        public bool IsChecked { get; set; }
+    }
+
+    public class CustomStepEntry {
+        public string CustomStep { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
